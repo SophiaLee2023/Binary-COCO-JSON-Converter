77e125216c1c24843634ebd964c7a50c8675dd69
Sophia Lee <sophia.anna.lee@gmail.com>
2026-06-10 13:46:52 -0400

    Edited for bulk annotations

--- main.py
+++ main.py
@@ -1,64 +1,66 @@
 import glob
 import json
 import os
-
 import cv2
 
-from src.create_annotations import (create_image_annotation, create_annotation_format, find_contours,
-                                    get_coco_json_format, create_category_annotation)
+from src.create_annotations import (create_image_annotation, 
+                                    create_annotation_format,
+                                    find_contours,
+                                    get_coco_json_format,
+                                    create_category_annotation)
 
-# Label ids of the dataset
+
 category_ids = {
-    "circle": 1,
-    "rectangle": 2,
+    'flood_water': 1,
 }
 
+
 MASK_EXT = 'png'
-ORIGINAL_EXT = 'png'
+ORIGINAL_EXT = 'jpg'
 
 
-# Get "images" and "annotations" info
-def images_annotations_info(maskpath):
-    annotation_id = 0
+def process_image(mask_path):
+    base = os.path.basename(mask_path).split('.')[0]
+    original_file = f'input/images/{base}.{ORIGINAL_EXT}'
+
+    mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+    height, width = mask.shape
+
+    coco = get_coco_json_format()
+    coco['categories'] = create_category_annotation(category_ids)
+
+    image_info = create_image_annotation(file_name=original_file, width=width, height=height)
+    coco['images'] = [image_info]
+    
+    _, thresh = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
     annotations = []
-    images = []
+    annotation_id = 1
 
-    for category in category_ids.keys():
-        for mask_image in glob.glob(os.path.join(maskpath, category, f'*.{MASK_EXT}')):
-            original_file_name = f'{os.path.basename(mask_image).split(".")[0]}.{ORIGINAL_EXT}'
-            mask_image_open = cv2.imread(mask_image)
-            height, width, c = mask_image_open.shape
+    for contour in contours:
+        ann = create_annotation_format(
+            contour,
+            image_info['id'],
+            category_ids,
+            annotation_id
+        )
+        if ann['area'] > 0:
+            annotations.append(ann)
+            annotation_id += 1
 
-            if original_file_name not in map(lambda img: img['file_name'], images):
-                image = create_image_annotation(file_name=original_file_name, width=width, height=height)
-                images.append(image)
-            else:
-                image = [element for element in images if element['file_name'] == original_file_name][0]
-
-            contours = find_contours(mask_image_open)
-
-            for contour in contours:
-                annotation = create_annotation_format(contour, image['id'], category_ids[category], annotation_id)
-                if annotation['area'] > 0:
-                    annotations.append(annotation)
-                    annotation_id += 1
-
-    return images, annotations, annotation_id
+    coco['annotations'] = annotations
+    return coco, len(annotations), base
 
 
 if __name__ == "__main__":
-    coco_format = get_coco_json_format()  # Get the standard COCO JSON format
+    os.makedirs('output', exist_ok=True)
+                
+    for mask_path in glob.glob(f'input/masks/*.{MASK_EXT}'):
+        coco_json, ann_count, base = process_image(mask_path)
 
-    for keyword in ["valid", "test", "train"]:
-        mask_path = f"dataset/{keyword}_mask/"
+        out_path = f'output/{base}.json'
+        with open(out_path, 'w') as f:
+            json.dump(coco_json, f, indent=4)
 
-        # Create category section
-        coco_format["categories"] = create_category_annotation(category_ids)
-
-        # Create images and annotations sections
-        coco_format["images"], coco_format["annotations"], annotation_cnt = images_annotations_info(mask_path)
-
-        with open(f"output/{keyword}.json", "w") as outfile:
-            json.dump(coco_format, outfile, sort_keys=True, indent=4)
-
-        print("Created %d annotations for images in folder: %s" % (annotation_cnt, mask_path))
+        print(f"Created {ann_count} annotations → {out_path}")
